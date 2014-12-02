d5f524b578e495212a972e1ecfcc4750d5156765
Jan Groothuijse <jan@groothuij.se>
2014-12-02 13:00:26 +0100

    Added beginning of translate/visitor

--- src/example/jdt/STest.scala
+++ src/example/jdt/STest.scala
@@ -15,6 +15,8 @@ import org.eclipse.jdt.core.dom.NormalAnnotation;
 import org.eclipse.jdt.core.dom.MemberValuePair
 import scala.io.Source
 
+import nl.ru.cs.ecalogic.ast._
+
 
 object STest {
   def main(args: Array[String]) {
@@ -28,10 +30,12 @@ object STest {
     val cu: CompilationUnit = parser.createAST(null).asInstanceOf[CompilationUnit];
     
     // give visitor a name, such that it can be recursive, because it needs to be.
- 
+    val p = new Program(Map[String, Import](), Map[String, FunDef](), Map[String, Definition]());
     cu.accept(new ASTVisitor() {
  
       var names: Set[String] = new HashSet();
+      
+      p.imports.++:("Hoi", Import([""]));
       
       override def visit(node: WhileStatement) : Boolean = {
         System.out.println("Usage of while detected; we should throw an error")
@@ -41,6 +45,8 @@ object STest {
       }
       
       override def visit(node: ForStatement) : Boolean = {
+        
+        
         System.out.println("Usage of for detected, investigating")
         node.accept(new ASTVisitor() {
           override def visit(node: VariableDeclarationExpression): Boolean = {
---
+++ src/main/scala/nl/ru/cs/ecalogic/translate/Visitor.scala
@@ -0,0 +1,30 @@
+package main.scala.nl.ru.cs.ecalogic.translate
+
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.SimpleName;
+import org.eclipse.jdt.core.dom.WhileStatement;
+import org.eclipse.jdt.core.dom.ForStatement;
+import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
+import org.eclipse.jdt.core.dom.VariableDeclarationExpression;
+import org.eclipse.jdt.core.dom.NormalAnnotation;
+import org.eclipse.jdt.core.dom.MemberValuePair
+
+/**
+ * Trait to define utility function we want to gather the results of visiting
+ */
+trait Visitor[A] {
+  /**
+   * To be called after the object has a chance to visit the nodes.
+   */
+  def result() : Option[A]
+}
+
+/**
+ * Default mock/stub implementation.
+ */
+class NotImplementedVisitor[A]() extends ASTVisitor with Visitor[A] {
+  def result() : Option[A] = Option.empty;
+}
