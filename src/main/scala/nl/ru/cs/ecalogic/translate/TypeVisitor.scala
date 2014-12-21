68be3d124fac8f04dfcdc91e6ff359ab666b1828
Jan Groothuijse <jan@groothuij.se>
2014-12-21 21:22:32 +0100

    Added documentation to TypeVisitor

--- src/main/scala/nl/ru/cs/ecalogic/translate/TypeVisitor.scala
+++ src/main/scala/nl/ru/cs/ecalogic/translate/TypeVisitor.scala
@@ -1,6 +1,23 @@
 package nl.ru.cs.ecalogic
 package translate
 
+/**
+ * Transform Java types to Ecalogic types
+ * 
+ * eca                      java
+ * 
+ * :: ASTType =
+ * |  ASTIntegerT           // int long char byte short
+ * |  ASTRealT              // float double
+ * |  ASTVoidT              // Void
+ * |  ASTArrayT ASTType     // build-in arrays of java, so the [] as in int[] float[] byte[] SomeType[]
+ * |  ASTPointerT ASTType   // Does not exist in java
+ * |  ASTStructT String     // Object
+ * |  ASTUnionT String      // Does not exist in java
+ * 
+ * We should ignore generics 
+ * 
+ */
 class TypeVisitor extends NotImplementedVisitor[ast.ASTType]{
   
 }
